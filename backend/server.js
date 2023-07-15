f03bd63e35d92893d86cc18484dffe47ca78a7aa
Yasir <99634661+Yasir-Rana@users.noreply.github.com>
2023-07-15 14:23:50 +0500

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 const port = 3000;
-const host = "0.0.0.0";   // Replace it with your IP Address
+const host = "10.8.33.0";   // Replace it with your IP Address
 const upload = multer({ dest: 'uploads/' });
 
 
